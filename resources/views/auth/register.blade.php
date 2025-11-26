7488f86674de797699118b893f3d0d70b84f6db7
Roni Hidayat <ronihidayat66.99@gmail.com>
2025-11-26 20:57:34 +0700

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,7 +22,7 @@
             padding: 30px;
             border-radius: 8px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            width: 300px;
+            width: 400px;
             text-align: center;
         }
 
