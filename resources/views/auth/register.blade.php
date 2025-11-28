19bd00f0aaa77c1d4d85e9c27748c30be1aa865a
Roni Hidayat <ronihidayat66.99@gmail.com>
2025-11-28 13:50:55 +0700

    merapikan view register dan login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,66 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
-    <style>
-        body {
-            font-family: sans-serif;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .kotak-register {
-            background: white;
-            padding: 30px;
-            border-radius: 8px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            width: 400px;
-            text-align: center;
-        }
-
-        .h2 {
-            align-items: center;
-        }
-
-        input {
-            width: 90%;
-            padding: 10px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        button {
-            width: 100%;
-            padding: 10px;
-            background-color: #28a745;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #218838;
-        }
-
-        .error-msg {
-            color: red;
-            font-size: 12px;
-            text-align: left;
-            margin-top: -5px;
-            margin-bottom: 10px;
-            display: block;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
 
 <body>
-    <div class="kotak-register">
+    <div class="kotak-form">
         <h1>Register Jadi Warga RT002/008 </h1>
         <h2>Selamat Bergabung Warga</h2>
 
