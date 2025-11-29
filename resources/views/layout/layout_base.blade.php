489b43c0e299ab4e21310b5e5bcf450c68df0858
Roni Hidayat <ronihidayat66.99@gmail.com>
2025-11-29 08:35:39 +0700

    update view admin

--- resources/views/layout/layout_base.blade.php
+++ resources/views/layout/layout_base.blade.php
@@ -6,27 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Admin Lapor Pak RT 002 RW 008</title>
-    <link rel="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
-        /* Sedikit CSS manual agar sidebar tingginya full layar */
         .sidebar {
             height: 100vh;
+            /* Full tinggi layar */
             background-color: #343a40;
+            /* Abu gelap */
+        }
+
+        /* Saya update sedikit biar hovernya lebih mulus */
+        .sidebar .nav-link {
             color: white;
+            margin-bottom: 5px;
         }
 
-        .sidebar a {
-            color: #fff;
-            text-decoration: none;
-            padding: 10px;
-            display: block;
-        }
-
-        .sidebar a:hover {
+        .sidebar .nav-link:hover {
             background-color: #495057;
+            border-radius: 5px;
         }
     </style>
-
 </head>
 
 <body>
@@ -34,19 +35,22 @@
     <div class="container-fluid">
         <div class="row">
 
-            <nav class="col-md-2 sidebar p-3">
-                <h4>Lapor Pak RT</h4>
-                <hr>
-                <ul class="nav flex columns">
+            <nav class="col-md-3 col-lg-2 d-md-block sidebar p-3 collapse">
+                <h4 class="text-white">Lapor Pak RT</h4>
+                <hr class="text-white">
+
+                <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a href="#"> Dashboard</a>
+                        <a class="nav-link active" href="#">Dashboard</a>
                     </li>
-                    <li class="van-item">
-                        <a href="#">inbox laporan Warga</a>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inbox Laporan Warga</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#">Data Warga</a>
+                        <a class="nav-link" href="#">Data Warga</a>
                     </li>
+
                     <li class="nav-item mt-5">
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
@@ -56,14 +60,13 @@
                 </ul>
             </nav>
 
-            <main class="col-md-10 ms-sm-auto col-lg-10 px-md-4 py-4">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
                 @yield('content')
             </main>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 
 </body>
 
